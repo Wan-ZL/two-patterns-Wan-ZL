811a69ba85323ed91a1599e4910d758c904d0c34
Wan-ZL <wanzelin007@gmail.com>
2017-02-05 21:47:57 -0700

    Random AI finished

--- src/model/RandomAI.java
+++ src/model/RandomAI.java
@@ -1,25 +1,43 @@
 package model;
 
 import java.awt.Point;
+import java.util.Random;
 
 /**
- * This strategy selects the first available move at random.  It is easy to beat
+ * This strategy selects the first available move at random. It is easy to beat
  * 
- * @throws IGotNowhereToGoException whenever asked for a move that is impossible to deliver
+ * @throws IGotNowhereToGoException
+ *             whenever asked for a move that is impossible to deliver
  * 
  * @author mercer
  */
 
-// There is an intentional compile time error.  Implement this interface
+// There is an intentional compile time error. Implement this interface
 public class RandomAI implements TicTacToeStrategy {
 
-  // Randomly find an open spot while ignoring possible wins and stops.
-  // This should be easy to beat as a human. 
+	// Randomly find an open spot while ignoring possible wins and stops.
+	// This should be easy to beat as a human.
 
-  @Override
-  public Point desiredMove(TicTacToeGame theGame) {
- 
- 
-    return new Point(2, 2);
-  }
+	@Override
+	public Point desiredMove(TicTacToeGame theGame) {
+		int emptyCounter = 0;
+		for(int i = 0 ; i < theGame.size() ; i++){	// check if the board is filled
+			for(int m = 0 ; m < theGame.size() ; m++){
+				if(theGame.getTicTacToeBoard()[i][m] == '_'){
+					emptyCounter++;
+				}
+			}
+		}
+		if(emptyCounter == 0){	//when empterCounter is 0, means no empty space
+			throw new IGotNowhereToGoException("the board is filled");
+		}
+		Random rand = new Random();
+		int x = rand.nextInt(3);
+		int y = rand.nextInt(3);
+		while(theGame.getTicTacToeBoard()[x][y] != '_'){
+			x = rand.nextInt(3);
+			y = rand.nextInt(3);
+		}
+		return new Point(x, y);
+	}
 }
--- src/model/StopperAI.java
+++ src/model/StopperAI.java
@@ -4,24 +4,28 @@ import java.awt.Point;
 import java.util.Random;
 
 /**
- * This TTT strategy tries to prevent the opponent from winning by checking
- * for a space where the opponent is about to win. If none found, it randomly
- * picks a place to win, which an sometimes be a win even if not really trying.
+ * This TTT strategy tries to prevent the opponent from winning by checking for
+ * a space where the opponent is about to win. If none found, it randomly picks
+ * a place to win, which an sometimes be a win even if not really trying.
  * 
  * @author mercer
  */
+
+/*
+ * 1. if can block, do it. 
+ * 2. if enter is empty, fill it. 
+ * 3. if can win, get it
+ * 4. if corner is empty, random choose one corner 
+ * 5. random choose one
+ */
 public class StopperAI implements TicTacToeStrategy {
-  
-  @Override
-  public Point desiredMove(TicTacToeGame theGame) {
 
-    // First look to block an opponent win
-
-    // If the AI can not block, look for a win
-
-    // If no block or win is possible, pick a move from those still available
-
-    return null;
-  }
+	@Override
+	public Point desiredMove(TicTacToeGame theGame) {
+		
+		
+		
+		return null;
+	}
 
 }
